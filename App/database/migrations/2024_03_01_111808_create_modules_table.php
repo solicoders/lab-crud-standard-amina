5d57c878283d7bf91356004f2a7a36c1e7164029
Amina Assaid <aminaassaid123@gmail.com>
2024-03-01 12:38:40 +0100

    migration

---
+++ App/database/migrations/2024_03_01_111808_create_modules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('competence_id');
+            $table->string('nom');
+            $table->string('description');
+            $table->foreign('competence_id')->references('id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modules');
+    }
+};
