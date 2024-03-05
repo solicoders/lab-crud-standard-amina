6b12cc9916676bd03ab071b08dab49516ed7e821
Amina Assaid <aminaassaid123@gmail.com>
2024-03-05 14:22:48 +0100

    fix package

--- App/app/Repositories/BaseRepository.php
+++ App/app/Repositories/BaseRepository.php
@@ -2,21 +2,30 @@
 
 namespace App\Repositories;
 
-use App\Models\Module;
 use Illuminate\Database\Eloquent\Model;
-abstract class BaseRepository {
 
+abstract class BaseRepository {
     protected $model;
 
-    public function __construct(Model $model) {
+    public function __construct(Model $model){
         $this->model = $model;
     }
-
-   public function create($data){
-     return $this->model->create($data);
-   }
-
-
-
+    public function paginatedData($perpage = 4){
+        return $this->model->paginate($perpage);
+    }
+    public function show($id)
+    {
+        return $fetcheddData = $this->model->findOrFail($id);
+    }
+    public function update($id ,$validatedData){
+        $toUpdate = $this->model->find($id);
+        $toUpdate->update($validatedData);
+    }
+    public function store(array $validatedData){
+        return $this->model->create($validatedData);
+    }
+    public function destroy($id){
+        $toDelete = $this->model->find($id);
+        return $toDelete->delete();
+    }
 }
-?>
